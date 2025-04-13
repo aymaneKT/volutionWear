017baabf7e0531a27370f340c33a5f1a8a85bb8b
aymaneKT <aymanekabtitech@gmail.com>
2025-04-13 15:39:14 +0200

    updated login and register form

--- Client/src/components/Register.tsx
+++ Client/src/components/Register.tsx
@@ -1,33 +1,43 @@
 import { useState } from "react";
-import img from "../VolutionWear.png";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { PiCoatHangerBold } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import Input from "./Button";
 
 type UserType = {
   username: string;
   name: string;
   surname: string;
-  email:string;
+  email: string;
   password: string;
   isSeller: boolean;
 };
 
 export default function Register() {
+  const [isPasswordVisible, setPasswordVisibility] = useState<boolean>(false);
   const [user, setUser] = useState<UserType>({
     username: "",
     name: "",
     surname: "",
-    email:"",
+    email: "",
     password: "",
     isSeller: false,
   });
   return (
-    <div className="flex  h-screen">
-      <div className="bg-[#F8F8F8] grow flex flex-col py-2 justify-center items-center min-[1120px]:w-[25%]">
-        <h1 className="text-4xl font-medium py-1 mt-5">CREATE ACCOUNT</h1>
-        <p className="text-[#A6A6A7] mb-3">Sign up and start your journey</p>
+    <div className="flex bg-[#F8F8F8]">
+      {/* COLONNA SINISTRA - FORM */}
+      <div className=" flex flex-col h-screen max-[480px]:h-auto max-[480px]:py-4   justify-center grow-2  items-center max-w-[100%]  max-[992px]:w-[100%]  max-[992px]:px-12 max-[480px]:px-0">
+        <h1
+          style={{ fontSize: "clamp(2rem, 3vw, 0.8rem" }}
+          className="font-medium"
+        >
+          CREATE ACCOUNT
+        </h1>
+        <p className="text-[#A6A6A7] mb-5">Sign up and start your journey</p>
 
-        <div className="self-center flex flex-col  w-[100%] px-4">
-          <div className="Username flex flex-col mb-1.5 gap-1.5">
+        <div className="self-center flex flex-col gap-2 w-[100%] px-4">
+          {/* Username */}
+          <div className="flex flex-col gap-1">
             <label htmlFor="username" className="font-medium">
               Username
             </label>
@@ -38,10 +48,12 @@ export default function Register() {
               id="username"
               placeholder="Enter your username"
               required
-              className="border-1 outline-0 pl-2.5 p-2 rounded-[8px] border-[#A6A6A7]"
+              className="border-1 outline-0 pl-2.5 p-2 rounded-[4px] border-[#E3E6E9] mb-2"
             />
           </div>
-          <div className="Name flex flex-col mb-1.5 gap-1.5">
+
+          {/* Name */}
+          <div className="flex flex-col gap-1.5">
             <label htmlFor="name" className="font-medium">
               Name
             </label>
@@ -52,10 +64,12 @@ export default function Register() {
               id="name"
               placeholder="Enter your name"
               required
-              className="border-1 outline-0 pl-2.5 p-2 rounded-[8px] border-[#A6A6A7]"
+              className="border-1 outline-0 pl-2.5 p-2 rounded-[4px] border-[#E3E6E9] mb-2"
             />
           </div>
-          <div className="Surname flex flex-col mb-1.5 gap-1.5">
+
+          {/* Surname */}
+          <div className="flex flex-col gap-1.5">
             <label htmlFor="surname" className="font-medium">
               Surname
             </label>
@@ -66,63 +80,92 @@ export default function Register() {
               id="surname"
               placeholder="Enter your surname"
               required
-              className="border-1 outline-0 pl-2.5 p-2 rounded-[8px] border-[#A6A6A7]"
+              className="border-1 outline-0 pl-2.5 p-2 rounded-[4px] border-[#E3E6E9] mb-2"
             />
           </div>
-          <div className="Surname flex flex-col mb-1.5 gap-1.5">
-            <label htmlFor="surname" className="font-medium">
+
+          {/* Email */}
+          <div className="flex flex-col gap-1.5">
+            <label htmlFor="email" className="font-medium">
               Email
             </label>
             <input
               onChange={(e) => setUser({ ...user, email: e.target.value })}
               value={user.email}
-              type="text"
-              id="surname"
-              placeholder="Enter your surname"
+              type="email"
+              id="email"
+              placeholder="Enter your email"
               required
-              className="border-1 outline-0 pl-2.5 p-2 rounded-[8px] border-[#A6A6A7]"
+              className="border-1 outline-0 pl-2.5 p-2 rounded-[4px] border-[#E3E6E9] mb-2"
             />
           </div>
-          <div className="Password flex flex-col mb-1.5 gap-1.5">
+
+          {/* Password con occhio */}
+          <div className="flex flex-col mb-5 gap-1.5 relative">
             <label htmlFor="password" className="font-medium">
               Password
             </label>
-            <input
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              value={user.password}
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              required
-              className="border-1 outline-0 pl-2.5 p-2 rounded-[8px] border-[#A6A6A7]"
-            />
+            <div className="flex items-center border-1">
+              <input
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                value={user.password}
+                type={isPasswordVisible ? "text" : "password"}
+                id="password"
+                placeholder="Enter your password"
+                required
+                className="border-1 outline-0 pl-2.5 border-r-0 grow-2 p-2 rounded-[4px] rounded-tr-0 rounded-br-0 border-[#E3E6E9]"
+              />
+              <div
+                className="cursor-pointer rounded-[4px] border-1 rounded-tl-0 rounded-bl-0 h-[100%] px-3 border-[#E3E6E9] flex items-center"
+                onClick={() => setPasswordVisibility(!isPasswordVisible)}
+              >
+                {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+              </div>
+            </div>
           </div>
-          <div className="flex items-center gap-2 my-2">
-            <p>Register as a seller ?</p>{" "}
+          <Input />
+          {/* Seller toggle */}
+          <div className="flex items-center gap-2 my-1">
+            <p>Register as a seller?</p>
             <label className="relative inline-flex items-center cursor-pointer">
               <input
                 type="checkbox"
                 checked={user.isSeller}
-                onChange={() => {
-                  setUser({ ...user, isSeller: !user.isSeller });
-                }}
+                onChange={() => setUser({ ...user, isSeller: !user.isSeller })}
                 className="sr-only peer"
               />
-              <div className="group peer bg-white rounded-full duration-300 w-16 h-8 ring-2 ring-red-500 after:duration-300 after:bg-red-500 peer-checked:after:bg-green-500 peer-checked:ring-green-500 after:rounded-full after:absolute after:h-6 after:w-6 after:top-1 after:left-1 after:flex after:justify-center after:items-center peer-checked:after:translate-x-8 peer-hover:after:scale-95" />
+              <div className="group peer bg-white rounded-full duration-300 w-16 h-8 ring-2 ring-red-500 after:duration-300 after:bg-red-500 peer-checked:after:bg-green-500 peer-checked:ring-green-500 after:rounded-full after:absolute after:h-6 after:w-6 after:top-1 after:left-1 peer-checked:after:translate-x-8 peer-hover:after:scale-95" />
             </label>
           </div>
-          <button className="bg-[#5805E9] text-white py-2 my-2 rounded-md hover:bg-[#EEE6FD] hover:text-[#5805E9] hover:border-[#5805E9] hover:border-[2px] hover:cursor-pointer transition-all transition-200">
+
+          {/* Register Button */}
+          <button className="bg-gradient-to-r from-purple-300 to-purple-700 text-white py-3 rounded-[8px] cursor-pointer transition-colors transition-200">
             Register
           </button>
-          <p className="text-center pb-4 ">
-            Already have an account?{" "}
-            <Link to="/login">
-              <span className="text-[#5805E9] cursor-pointer">Log in</span>
-            </Link>
-          </p>
         </div>
+
+        {/* Bottom */}
+        <p className="text-center absolute bottom-1 max-[992px]:relative max-[992px]:my-2">
+          Already have an account?{" "}
+          <Link to="/login">
+            <span className="text-[#5805E9] cursor-pointer">Log in</span>
+          </Link>
+        </p>
       </div>
-      <img src={img} alt="Logo Image" className="max-[1110px]:hidden grow" />
+
+      {/* COLONNA DESTRA */}
+      <div className="max-[992px]:hidden bg-[#B074F3]  grow flex flex-col py-8 items-center">
+        <h1 className="text-center text-white text-[1rem] p-6 font-bold uppercase">
+          #withVolutionwear
+          <br />
+          Buy, sell, and discover unique fashion - sustainably. <br />
+          Join the movement and refresh your wardrobe, one piece at a time.{" "}
+          <br />
+          Trusted by thousands | Zero fees | Always in style
+        </h1>
+        <PiCoatHangerBold className="text-white text-[20rem] rotate-12" />
+        <p className="text-white text-5xl mt-2.5">Volution Wear</p>
+      </div>
     </div>
   );
 }
